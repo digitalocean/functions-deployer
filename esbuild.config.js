e82895b891953edd097f0ba3ec005142fe32e1fc
James Thomas <james@jamesthom.as>
2021-05-05 12:25:42 +0100

    Initial commit - moving files from CLI repo.

---
+++ esbuild.config.js
@@ -0,0 +1,19 @@
+const esbuild = require('esbuild')
+
+// Automatically exclude all node_modules from the bundled version
+const { nodeExternalsPlugin } = require('esbuild-node-externals')
+
+esbuild.build({
+  // traverse dependency tree for bundling from this file
+  entryPoints: ['./src/index.ts'],
+  // built version of the library
+  outfile: 'lib/index.js',
+  // bundle & minify output file
+  bundle: true,
+  minify: false,
+  platform: 'node',
+  sourcemap: true,
+  target: 'node14',
+  external: ['*.html'],
+  plugins: [nodeExternalsPlugin()]
+}).catch(() => process.exit(1))
