f1410b6cc10700bc5b161c6df4221b4ad5c7941f
Davi DeBarros <davibarros1994@gmail.com>
2022-11-14 16:44:59 -0500

    Updates lint script format (#22)

--- scripts/prettier-check.sh
+++ scripts/prettier-check.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 printf "Running prettier --list-different\n\n"
 if ! ./node_modules/.bin/prettier --list-different './**/*.{js,jsx,ts,tsx,json}'; then
-  printf "\nUnformatted files found by Prettier. Please run 'yarn format' and commit the changes.\n\n"
+  printf "\nUnformatted files found by Prettier. Please run 'npm run format' and commit the changes.\n\n"
   exit 1
 else
   printf "No improperly formatted files found by Prettier.\n"
