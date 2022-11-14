1292082cd0ce0f5561695134fbea00cbf1e27939
Davi DeBarros <davibarros1994@gmail.com>
2022-11-14 14:23:09 -0500

    E2e trigger tests (#19)
    * Adds DO token to credentails to be submitted as part of the remote build
    
    * Updates from PR review
    
    * Adds triggers e2e tests
    
    * Updates package.lock
    
    * Updates the env vars file
    
    * Updates the env vars file
    
    * Uses tap format output
    
    * Updates bats to latest version
    
    * Updates package.lock

---
+++ e2e/test_helper.bash
@@ -0,0 +1,37 @@
+load ../../node_modules/bats-support/load.bash
+load ../../node_modules/bats-assert/load.bash
+
+## loads values from .env.test file and validates that required
+## envs have been provided
+if [ -f ../.env.test ]; then
+  export $(echo $(cat ../.env.test | sed 's/#.*//g' | xargs) | envsubst)
+fi
+
+if [ -z "$DO_API_KEY" ]; then
+  kill $PPID
+  echo "Missing DO_API_KEY"
+elif [ -z "$TEST_NAMESPACE" ]; then
+  kill $PPID
+  echo "Missing TEST_NAMESPACE"
+fi
+
+if [ -z "$DOSLS" ]; then
+  DOSLS=../bin/run
+fi
+
+if [ -z "$DOCTL" ]; then
+  DOCTL=../bin/doctl
+fi
+
+delete_package() {
+  $DOCTL sls undeploy $1 --packages
+}
+
+init_namespace() {
+  $DOCTL auth init $DO_API_KEY
+  $DOCTL sls install
+  $DOCTL sls connect $TEST_NAMESPACE
+
+  ## Ideal way to auth the deployer
+  # export NIMBELLA_DIR=bash -c "$DOCTL sls status --credentials | jq -r '.path'"
+}
---
+++ scripts/getDoctl.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Obtains the latest version of doctl for the os and arch of the machine it is running on.
+# Assumes running under bash even on windows.
+
+# The following is heuristic and incomplete.  A better way would be welcome.
+which uname >/dev/null || (echo "Cannot run on native windows" && exit 1)
+
+set -e
+
+SUFFIX="tar.gz"
+UOS=$(uname -s)
+UNPACK="tar xzf"
+if [[ "$UOS" == *Linux* ]]; then
+  OS=linux
+elif [ "$UOS" == Darwin ]; then
+  OS=darwin
+else
+  OS=windows
+  UNPACK=unzip
+  SUFFIX=zip
+fi
+
+UARCH=$(uname -m)
+if [ $UARCH == x86_64 ]; then
+  ARCH=amd64
+elif [[ $UARCH == *386* ]]; then
+  ARCH=386
+else
+  ARCH=$UARCH
+fi
+
+# Gets the latest release of doclt using the GitHub API
+# and saves it in bin directory.
+VERSION=$(curl --silent "https://api.github.com/repos/digitalocean/doctl/releases/latest" | jq -r .tag_name | sed 's/v//')
+NAME="doctl-$VERSION-$OS-$ARCH.$SUFFIX"
+DOWNLOAD_URL="https://github.com/digitalocean/doctl/releases/download/v$VERSION/$NAME"
+
+echo "- Downloading from $DOWNLOAD_URL"
+[ -d bin ] || mkdir bin
+curl -LO --silent "$DOWNLOAD_URL"
+$UNPACK "$NAME" -C bin
+rm "$NAME"
+echo -e "\xE2\x9C\x94 Downloaded doctl version v$VERSION"
