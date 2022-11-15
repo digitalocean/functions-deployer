a2be6419a4fea50c545a9d67242a2782933971de
Davi DeBarros <ddebarros@digitalocean.com>
2022-11-15 14:37:42 -0500

    test env vars

--- e2e/test_helper.bash
+++ e2e/test_helper.bash
@@ -7,6 +7,7 @@ if [ -f ../.env.test ]; then
   export $(echo $(cat ../.env.test | sed 's/#.*//g' | xargs) | envsubst)
 fi
 
+echo "foo is $FOO"
 if [ -z "$DO_API_KEY" ]; then
   kill $PPID
   echo "Missing DO_API_KEY"
