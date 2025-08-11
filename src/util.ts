8d3a010d74b0044709d679ee6c25450dc932ba56
code-by-jassu <jaswanthkosanam@gmail.com>
2025-08-11 12:51:34 +0530

    added authTagLength during decryption

--- src/util.ts
+++ src/util.ts
@@ -176,7 +176,8 @@ export function decryptProjectConfig(
     const decipher = crypto.createDecipheriv(
       'aes-256-gcm',
       key,
-      Buffer.from(ivHex, 'hex')
+      Buffer.from(ivHex, 'hex'),
+      { authTagLength: 16 }
     );
     decipher.setAuthTag(Buffer.from(tagHex, 'hex'));
     const decrypted = Buffer.concat([
